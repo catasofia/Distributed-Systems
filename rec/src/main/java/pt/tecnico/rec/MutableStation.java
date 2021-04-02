8706b5d9a1f90454aca86c510c949bf5928c0f76
ist193695 <catarinasousa2000@tecnico.ulisboa.pt>
2021-04-02 15:05:18 +0100

    creating pairs for mutable variables.

--- rec/src/main/java/pt/tecnico/rec/MutableStation.java
+++ rec/src/main/java/pt/tecnico/rec/MutableStation.java
@@ -1,12 +1,23 @@
 package pt.tecnico.rec;
 
 import java.util.*;
+import org.apache.commons.lang3.tuple.MutablePair;
 
 public class MutableStation{
 
-    private String abbr;
-    private ArrayList<Boolean> docksAvailability = new ArrayList<Boolean>();
+    private static MutablePair<String, ArrayList<Boolean>> _docksAvailability = new MutablePair<String, ArrayList<Boolean>>();
 
-    public MutableStation(){
+    public MutableStation(String abbr){
+        _docksAvailability.setLeft(abbr);
+        _docksAvailability.setRight(new ArrayList<Boolean>());
+    }
+
+    public ArrayList<Boolean> getDocksAvailability(){
+        return _docksAvailability.getRight();
+    }
+
+    public void setDockAvailability(Integer dockNr){
+        Boolean availability = _docksAvailability.getRight().get(dockNr);
+        _docksAvailability.getRight().set(dockNr, !availability);
     }
 }
--- rec/src/main/java/pt/tecnico/rec/MutableUser.java
+++ rec/src/main/java/pt/tecnico/rec/MutableUser.java
@@ -1,12 +1,23 @@
 package pt.tecnico.rec;
 
 import java.util.*;
+import org.apache.commons.lang3.tuple.MutablePair;
 
 public class MutableUser{
 
-    private HashMap<String, Integer> balance = new HashMap<String, Integer>();
+    private static MutablePair<String, Integer> _balance = new MutablePair<String, Integer>();
 
     public MutableUser(String id){
-        balance.put(id, 0);
+        _balance.setLeft(id);
+        _balance.setRight(0);
+    }
+
+    public Integer getBalance(){
+        return _balance.getRight();
+    }
+
+    public void increaseBalance(Integer balance){
+        Integer previousBalance = _balance.getValue();
+        _balance.setValue(previousBalance + balance);
     }
 }
