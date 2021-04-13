997d5ab00cef8c0e08bde90b978aa06e2a749d1b
amariaribeiro <ist193735@tecnico.ulisboa.pt>
2021-04-13 17:55:02 +0100

    MutableStation changes

--- rec/src/main/java/pt/tecnico/rec/MutableStation.java
+++ rec/src/main/java/pt/tecnico/rec/MutableStation.java
@@ -5,61 +5,39 @@ import org.apache.commons.lang3.tuple.MutablePair;
 
 public class MutableStation{
 
-    private static MutablePair<String, Boolean[]> _docksAvailability = new MutablePair<>();
-    //null == there is an available bike
     private Integer _deliveries;
     private Integer _requisitions;
+    private Integer _docks;
+    private Integer _bikes;
+
 
     public MutableStation(String abbr){
-        _docksAvailability.setLeft(abbr + "/docksAvailability");
-        /*Boolean[] docks = new Boolean[];
-        _docksAvailability.setRight(docks);*/
         _deliveries = 0;
         _requisitions = 0;
+        _docks = 0;
+        _bikes = 0;
     }
 
     public MutableStation(String abbr, Integer docksNr, Integer bikesNr){
-        _docksAvailability.setLeft(abbr + "/docksAvailability");
         _requisitions = docksNr - bikesNr;
         _deliveries = 0;
-        Boolean[] docks = new Boolean[docksNr];
-        for(int i = 0; i < (docksNr - bikesNr); i++){
-            docks[i] = true;
-        }
-        _docksAvailability.setRight(docks);
+        _docks = docksNr;
+        _bikes = bikesNr;
     }
 
-    /*public ArrayList<Boolean> getDocksAvailability(){
-        return _docksAvailability.getRight();
-    }*/
-
-    public Boolean[] getDocksAvailability(){
-        return _docksAvailability.getRight();
-    }
-
-    public void setDockAvailability(Integer dockNr){
-        //Boolean availability = _docksAvailability.getRight().get(dockNr);
-        Boolean availability = _docksAvailability.getRight()[dockNr];
-        _docksAvailability.getRight()[dockNr] = !availability;
-    }
 
     public Integer getAvailableBikesNr(){
-        /*Integer count = 0;
-        //List<Boolean> availability = _docksAvailability.getRight();
-        Boolean[] availability = _docksAvailability.getRight();
-        for (Boolean bool: availability) {
-            if (bool == null) count++;
-        }
-        return count;*/
-        return (_docksAvailability.getRight().length - _requisitions + _deliveries);
+        return _bikes;
     }
 
-    public void addDeliveries(){
+    public void bikeDown(){
+        _bikes++;
         _deliveries++;
     }
 
-    public void addRequisitions(){
+    public void bikeUp(){
         _requisitions++;
+        _bikes--;
     }
 
     public Integer getDeliveries() {
